b511377a3667fbd844a08f9583f0b30551d3f8ed
Thomas Nguyen <thomas.nguyen@darenft.com>
2024-03-18 11:43:20 +0700

    remove unused interfaces

--- contracts/interfaces/IFreeMintable.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-interface IFreeMintable {
-  function freeMint(address to) external payable returns (uint256);
-}
--- contracts/interfaces/IInitializableCollection.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-interface IInitializableCollection {
-  function initialize(address owner, string calldata name, string calldata symbol, bytes calldata settings) external;
-}
